d5f0bcf590083df86c2186fd27616c238a2391fb
David Manthey <manthey@orbitals.com>
2025-03-15 08:25:26 -0400

    Reduce duplication

--- app/src/androidTest/java/com/orbitals/colorfilter/MainActivityUITest.java
+++ app/src/androidTest/java/com/orbitals/colorfilter/MainActivityUITest.java
@@ -1,15 +1,24 @@
 package com.orbitals.colorfilter;
 
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.util.Log;
 
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.By;
@@ -17,6 +26,7 @@ import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject2;
 import androidx.test.uiautomator.Until;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -50,24 +60,25 @@ public class MainActivityUITest {
         assertThat(launcherPackage, notNullValue());
         device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
 
+        // Initialize Espresso Intents
+        Intents.init();
         // Launch the app
         Context context = ApplicationProvider.getApplicationContext();
         final Intent intent = context.getPackageManager()
                 .getLaunchIntentForPackage(PACKAGE_NAME);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);    // Clear out any previous instances
         context.startActivity(intent);
+        device.wait(Until.hasObject(By.pkg(PACKAGE_NAME)), LAUNCH_TIMEOUT);
+        handlePermissionDialogs();
+    }
 
-        // Wait for the app to appear
-        device.wait(Until.hasObject(By.pkg(PACKAGE_NAME).depth(0)), LAUNCH_TIMEOUT);
+    @After
+    public void cleanup() {
+        Intents.release();
     }
 
     @Test
     public void testCameraAndFilterButtons() {
-        // Wait for the app to fully load
-        device.wait(Until.hasObject(By.pkg(PACKAGE_NAME)), LAUNCH_TIMEOUT);
-
-        handlePermissionDialogs();
-
         UiObject2 cameraButton = device.wait(
                 Until.findObject(By.res(PACKAGE_NAME, "switchCameraButton")), 2000);
         cameraButton.click();
@@ -79,6 +90,68 @@ public class MainActivityUITest {
                 Until.findObject(By.res(PACKAGE_NAME, "filterButton")), 2000);
         filterButton.click();
         Log.d(TAG, "Clicked filter button");
+    }
+
+    @Test
+    public void testImageModeFeatures() {
+        // 1. Set up intent response for the gallery picker
+        // Create a sample bitmap to return as the selected image
+        Bitmap sampleBitmap = BitmapFactory.decodeResource(
+                ApplicationProvider.getApplicationContext().getResources(),
+                android.R.drawable.ic_menu_report_image); // Using a system resource as sample
+
+        // Create a result intent with the sample image
+        Intent resultData = new Intent();
+        resultData.setData(Uri.parse("content://media/external/images/media/123")); // Dummy URI
+
+        // Set up the intent result
+        Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(
+                Activity.RESULT_OK, resultData);
+
+        // Stub the intent
+        intending(hasAction(Intent.ACTION_PICK)).respondWith(result);
+
+        // 2. Click the load image button using UiAutomator
+        UiObject2 loadImageButton = device.wait(
+                Until.findObject(By.res(PACKAGE_NAME, "loadImageButton")), 2000);
+        loadImageButton.click();
+
+        // 3. Verify the gallery intent was sent
+        intended(hasAction(Intent.ACTION_PICK));
+
+        // 4. Wait for image to load
+        device.wait(Until.hasObject(By.pkg(PACKAGE_NAME)), 2000);
+
+        // 5. Test pinch-to-zoom on the image
+        UiObject2 textureView = device.wait(
+                Until.findObject(By.res(PACKAGE_NAME, "textureView")), 2000);
+
+        textureView.pinchOpen(0.20f);
+
+        device.wait(Until.hasObject(By.pkg(PACKAGE_NAME)), 1000);
+
+        // 6. Test filter button in image mode
+        UiObject2 filterButton = device.wait(
+                Until.findObject(By.res(PACKAGE_NAME, "filterButton")), 2000);
+        filterButton.click();
+
+        // Wait for filter to apply
+        device.wait(Until.hasObject(By.pkg(PACKAGE_NAME)), 1000);
+
+        UiObject2 bctSeekBar = device.wait(
+                Until.findObject(By.res(PACKAGE_NAME, "bctSeekBar")), 2000);
+        int width = bctSeekBar.getVisibleBounds().width();
+        bctSeekBar.drag(new android.graphics.Point(width / 2, 0), 30);
+
+        // Wait for the change to apply
+        device.wait(Until.hasObject(By.pkg(PACKAGE_NAME)), 1000);
+
+        UiObject2 cameraButton = device.wait(
+                Until.findObject(By.res(PACKAGE_NAME, "switchCameraButton")), 2000);
+        cameraButton.click();
+
+        // Wait to confirm we're back to camera mode
+        device.wait(Until.hasObject(By.pkg(PACKAGE_NAME)), 2000);
     }
 
     private void handlePermissionDialogs() {
